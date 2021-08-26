a5230befd3497638efb9bb7ea331c1318ce3bfdf
BalaskandanGiri <balaskandangiri@gmail.com>
2021-08-26 23:54:17 +0530

    Update ValidateProduct.java

--- src/main/java/com/intuit/craft/services/ValidateProduct.java
+++ src/main/java/com/intuit/craft/services/ValidateProduct.java
@@ -1,29 +1,39 @@
 package com.intuit.craft.services;
 
+import com.intuit.craft.Exceptions.BadRequest;
+import com.intuit.craft.data.validate;
 import com.intuit.craft.entities.Product;
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.client.RestTemplate;
 
-public class ValidateProduct implements Runnable {
+@Data
+public class ValidateProduct implements Runnable{
 
     private final int MAX_RETRIES = 3;
 
     @Autowired
     ProductService productService;
 
+    Thread thread;
     private int currentRetries = 0;
     private Product product = null;
+    RestTemplate restTemplate;
 
     public ValidateProduct(Product product) {
         this.product = product;
+        restTemplate = new RestTemplate();
+        thread = Thread.currentThread();
     }
 
     @Override
     public void run() {
         while(true) {
             if (this.currentRetries >=  MAX_RETRIES) {
-                throw new RuntimeException("Validation failed for product " + product.getName() + ".");
+                this.thread.interrupt();
             }
-            if(productService.validate(this.product)) {
+            validate v = restTemplate.getForObject("http://localhost:9000/product/validate", validate.class);
+            if (v != null && v.getIsValid()) {
                 return;
             }
             this.currentRetries += 1;
