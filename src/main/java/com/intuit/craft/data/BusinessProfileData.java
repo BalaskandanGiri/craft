1578ad05af5e8d85354ab3463a4475023b54ae84
BalaskandanGiri <balaskandangiri@gmail.com>
2021-08-26 17:19:56 +0530

    adding data layer

--- src/main/java/com/intuit/craft/data/BusinessProfileData.java
+++ src/main/java/com/intuit/craft/data/BusinessProfileData.java
@@ -19,6 +19,7 @@ public class BusinessProfileData {
     @NotBlank(message = "Company name is mandatory")
     @Size(max = 255, message = "Company name must be between {min} and {max} characters long")
     private String companyName;
+
     @NotBlank(message = "Legal name is mandatory")
     @Size(max = 255, message = "Legal name must be between {min} and {max} characters long")
     private String LegalName;
@@ -26,8 +27,15 @@ public class BusinessProfileData {
     private Address businessAddress;
 
     private Address legalAddress;
+
+    @NotBlank(message = "Tax identifier name is mandatory")
     private String taxIdentifier;
+
+    @NotBlank(message = "Email is mandatory")
+    @Size(max = 255, message = "Email must be between {min} and {max} characters long")
     private String email;
+
     private String website;
+
     private List<String> products;
 }
