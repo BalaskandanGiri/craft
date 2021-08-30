b0f46e769e6e57ac6fcdb7f0ddd7b5a5a68172c4
BalaskandanGiri <balaskandangiri@gmail.com>
2021-08-30 15:24:28 +0530

    CORS

--- src/main/java/com/intuit/craft/controllers/BusinessProfileController.java
+++ src/main/java/com/intuit/craft/controllers/BusinessProfileController.java
@@ -5,6 +5,7 @@ import com.intuit.craft.entities.BusinessProfile;
 import com.intuit.craft.services.BusinessProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("businessProfile")
+@CrossOrigin(origins = "*")
 public class BusinessProfileController {
 
     @Autowired BusinessProfileService businessProfileService;
--- src/main/java/com/intuit/craft/controllers/ProductController.java
+++ src/main/java/com/intuit/craft/controllers/ProductController.java
@@ -6,6 +6,7 @@ import com.intuit.craft.entities.Product;
 import com.intuit.craft.services.ProductService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping(value = "product")
+@CrossOrigin(origins = "*")
 public class ProductController {
 
     @Autowired
